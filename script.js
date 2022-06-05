92f8c97a1c2b539ef9a5de4a04e3958fb286b0e9
Bradley Owens <bradowens17@hotmail.com>
2022-06-05 17:40:05 +1000

    created start of application where the input for username and password will create a user and add it to the members

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const createLogin = document.getElementById("login");
+
+const userName = document.querySelector(".input-username");
+
+const pin = document.querySelector(".input-password");
+
+const userContainer = document.querySelector(".user-container");
+
+const user = function (userName, password) {
+  this.userName = userName;
+  this.password = password;
+};
+
+const createdUsers = [];
+let members;
+
+createLogin.addEventListener("click", function () {
+  //Creates new user
+  const newUser = new user(userName.value, pin.value);
+
+  //Adds to user group
+  createdUsers.push(newUser);
+
+  // gets each users name
+  createdUsers.forEach(function (member) {
+    members = member.userName;
+  });
+
+  // adds to display of memebers
+  let users = document.createElement("p");
+  users.classList.add("my-users");
+  users.innerText = members;
+  userContainer.appendChild(users);
+});
