18e5820fe1e09bb6977461031a5b036556047de1
Bradley Owens <bradowens17@hotmail.com>
2022-06-14 10:16:16 +1000

    finish verification feature on user create with username,password and chosen language

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 const createLogin = document.getElementById("login");
 const userName = document.querySelector(".input-username");
 const userPin = document.querySelector(".input-password");
+const userLanguage = document.querySelector(".input-language");
 const languageChoice = document.querySelector(".input-language");
 const userContainer = document.querySelector(".user-container");
 const memberNumber = document.querySelector(".members-number");
@@ -13,6 +14,7 @@ let createdUsers = [];
 let members;
 let verifiedUserName;
 let verifiedPin;
+let verifiedLanguage;
 let numbers = /[0-9]/g;
 let upperCaseLetters = /[A-Z]/g;
 let symbols = "!`@#$ %^&*()+=-[]\\';,./{}|\":<>? ~_";
@@ -27,7 +29,7 @@ const resetInput = () => {
   languageChoice.value = "";
 };
 
-// Verify Username & Password
+// Verify Username & Password & Language
 
 const verifyUserName = function (username) {
   if (!username.match(upperCaseLetters)) {
@@ -47,6 +49,15 @@ const verifyPassword = function (pin) {
   }
 };
 
+const verifyLanguage = function (language) {
+  if (language === "") {
+    alert("Please choose a language to complete login");
+    createLogin.href = "#";
+  } else {
+    verifiedLanguage = language;
+  }
+};
+
 //  Using class for created users
 const user = class {
   constructor(userName, password, language) {
@@ -60,12 +71,11 @@ createLogin.addEventListener("click", function () {
   //Verfication
   verifyUserName(userName.value);
   verifyPassword(userPin.value);
+  verifyLanguage(userLanguage.value);
 
   //Creates new user
-  const newUser = new user(verifiedUserName, verifiedPin, languageChoice.value);
+  const newUser = new user(verifiedUserName, verifiedPin, verifiedLanguage);
 
-  console.log(newUser);
-  console.log(createdUsers);
   //Adds to user group
   createdUsers.push(newUser);
 
